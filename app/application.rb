83f3d2e093ad5e9ffd5b34ff45fa19d068d7502f
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-30 12:34:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,6 +10,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+      binding.pry
       item = @@items.find{|item| item.name == item_name}
       if item != nil
         resp.write "#{item.price}"
