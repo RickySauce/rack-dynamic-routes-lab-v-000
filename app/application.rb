f06ab490785c8764412570f7e44de0d0891d0f94
Eric DeSimone <eric.desimone@cortland.edu>
2018-06-30 12:33:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     @@items = []
 
     if req.path.match(/items/)
-      item_name = req.path.split("/items").last
+      item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
       if item != nil
         resp.write "#{item.price}"
